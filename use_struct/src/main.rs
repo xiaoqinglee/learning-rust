9baa5b0b8c74584dc34ed57775e24eb2733c537a
李小庆 <xiaoqinglee@foxmail.com>
2023-02-11 19:55:30 +0800

    associated function

--- use_struct/src/main.rs
+++ use_struct/src/main.rs
@@ -104,6 +104,75 @@ fn use_debug_macro() {
     dbg!(&a);
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// 可以分成多个impl块
+impl Rectangle {
+    // 允许method和field同名, 实践上常常将同名field设计成私有的, 将同名method设计成pub的, 后者是前者的getter
+    fn width(&self) -> u32 {
+        self.width
+    }
+
+    //&self 实际上是 self: &Self 的缩写
+    fn get_width(&self) -> u32 {
+        //
+        self.width
+    }
+
+    //&mut self 实际上是 self: &mut Self 的缩写
+    fn set_width(&mut self, new_width: u32) {
+        self.width = new_width;
+    }
+
+    //通过仅仅使用 self 作为第一个参数来使方法获取实例的所有权是很少见的；
+    // 这种技术通常用在当方法将 self 转换成别的实例的时候，这时我们想要防止调用者在转换之后使用原始的实例。
+
+    //Rust 有一个叫 自动引用和解引用（automatic referencing and dereferencing）的功能。
+    // 方法调用是 Rust 中少数几个拥有这种行为的地方。
+    // 它是这样工作的：
+    // 当使用 object.something() 调用方法时，
+    // Rust 会自动为 object 添加 &、&mut 或 * 以便使 object 与方法签名匹配。
+}
+
+impl Rectangle {
+    //所有在 impl 块中定义的函数被称为关联函数（associated function），
+    // 因为它们与 impl 后面命名的类型相关。
+    //
+    // 我们可以定义不以 self 为第一参数的关联函数（因此不是方法），
+    // 因为它们并不作用于一个结构体的实例。
+    //
+    // 关联函数经常被用作返回一个结构体新实例的构造函数。
+
+    fn new(width: u32, height: u32) -> Rectangle {
+        Rectangle { width, height }
+    }
+
+    fn new_square(side: u32) -> Rectangle {
+        Rectangle {
+            width: side,
+            height: side,
+        }
+    }
+}
+
+fn use_associated_func() {
+    //::语法用于关联函数和模块创建的命名空间
+    let mut a = Rectangle::new(2, 3);
+    println!("a {:?}", a);
+    println!("a.width {:?}", a.width);
+    println!("a.width() {:?}", a.width());
+    println!("a.get_width() {:?}", a.get_width());
+    a.set_width(1);
+    println!("a.get_width() {:?}", a.get_width());
+
+    let a = Rectangle::new_square(2);
+    println!("a {:?}", a)
+}
+
 fn main() {
-    use_debug_macro();
+    use_associated_func();
 }
