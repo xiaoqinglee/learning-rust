e9febec5045313bcee067328ffdbf56e86cd81b9
lixiaoqing <xiaoqinglee@foxmail.com>
2023-05-03 17:19:18 +0800

    fn

--- closure_and_iterator/src/main.rs
+++ closure_and_iterator/src/main.rs
@@ -67,6 +67,14 @@ fn fn_vs_closure() {
 // and if all values it captures by copy or move are Clone or Copy, respectively.
 
 fn test_closure() {
+    fn add_one(x: usize) -> usize {
+        x + 1
+    }
+    let ptr: fn(usize) -> usize = add_one;
+    assert_eq!(ptr(5), 6);
+    let clos: fn(usize) -> usize = |x| x + 5;
+    assert_eq!(clos(5), 10);
+
     fn fn_instance(i: i64) -> i64 {
         i + 1
     }
